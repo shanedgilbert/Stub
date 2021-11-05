97d7aafea2cf73e7232ba34a1cabc7067e6681a4
scottsak <48599109+scottsak@users.noreply.github.com>
2021-11-04 21:52:11 -0700

    made changes

---
+++ index.js
@@ -0,0 +1,28 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+
+
+
+
+
+
+app.get("/", function(req, res){
+   res.render("home");
+});
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
