9269d7ceb9f3383f684665e878d2bf91eebf39ce
Bryan Moore <31421559+bryanleemoore@users.noreply.github.com>
2022-01-31 15:41:14 -0800

    login actions inital commit

---
+++ client/src/actions/login.js
@@ -0,0 +1,25 @@
+import FacebookLogin from 'react-facebook-login'
+import React from "react";
+export const apiKey = "1376624219436332"
+
+const responseFacebook = (response) => 
+{
+  console.log(response);
+}
+
+const componentClicked = () => 
+{
+  console.log( "Click." )
+}
+
+export const LoginButton = () => (
+    <FacebookLogin
+      appId={apiKey}
+      fields="name,email,picture"
+      onClick={componentClicked}
+      callback={responseFacebook}
+      icon="fa-facebook"/>
+    )
+
+
+
