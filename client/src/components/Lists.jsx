8b540056462a2d59400dca39c7e52ca1066af2ac
scottsak <48599109+scottsak@users.noreply.github.com>
2022-02-01 01:59:10 -0800

    added more pictures

--- client/src/components/Lists.jsx
+++ client/src/components/Lists.jsx
@@ -15,29 +15,29 @@ function Lists() {
             imgURL= {tvshows[0].imgURL}
           />
             <List 
-            imgURL= {tvshows[0].imgURL}
+            imgURL= {tvshows[1].imgURL}
           />
             <List 
-            imgURL= {tvshows[0].imgURL}
+            imgURL= {tvshows[2].imgURL}
           />
             <List 
-            imgURL= {tvshows[0].imgURL}
+            imgURL= {tvshows[3].imgURL}
           />
           <a href="#section2" class="arrow__btn">›</a>
         </section>
         <section id="section2">
           <a href="#section1" class="arrow__btn">‹</a>
           <List 
-            imgURL= {tvshows[0].imgURL}
+            imgURL= {tvshows[4].imgURL}
           />
             <List 
-            imgURL= {tvshows[0].imgURL}
+            imgURL= {tvshows[5].imgURL}
           />
             <List 
-            imgURL= {tvshows[0].imgURL}
+            imgURL= {tvshows[6].imgURL}
           />
             <List 
-            imgURL= {tvshows[0].imgURL}
+            imgURL= {tvshows[7].imgURL}
           />
           <a href="#section3" class="arrow__btn">›</a>
         </section>
