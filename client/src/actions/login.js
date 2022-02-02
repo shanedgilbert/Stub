081943d18f5c25aed49f386e6d0f6b8697503290
Bryan Moore <31421559+bryanleemoore@users.noreply.github.com>
2022-02-01 18:42:56 -0800

    added getLocalUser, setLocalUser, and isLoggedIn functions

--- client/src/actions/login.js
+++ client/src/actions/login.js
@@ -7,7 +7,33 @@ const componentClicked = () =>
   console.log("Clicked");
 }
 
+export const getLocalUser = () => 
 {
+  if(localStorage.getItem('userLoginData'))
+  {
+    return JSON.parse(localStorage.getItem('userLoginData'));
+  }
+  else
+  {
+    return null;
+  }
+}
+
+export const setLocalUser = (userData) => 
+{
+  localStorage.setItem('userLoginData', JSON.stringify(userData));
+}
+
+export const isLoggedIn = () =>
+{
+  if(localStorage.getItem('userLoginData'))
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
 
 export const LoginButton = ({responseFacebook}) => (
