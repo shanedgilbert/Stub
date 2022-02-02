1252a0d9b9388aa5639cc925ff37fe733a6ba94f
Bryan Moore <31421559+bryanleemoore@users.noreply.github.com>
2022-02-01 18:43:52 -0800

    cleanup, fix imports

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,18 +1,17 @@
-import React,{ useState, useEffect } from "react";
-import { componentClicked, LoginButton,UserScreen } from "./actions/login";
+import React,{ useState } from "react";
+import {  LoginButton,getLocalUser,setLocalUser } from "./actions/login";
+
 
 
 
 
 function Login() {
- 
   return (
       <div class="container">
         <div class="row align-items-center my-5">
           <div class="col-lg-7">
           <h1 class="font-weight-light">Sign In</h1>
             <p>
-                <LoginButton/>                
             </p>
           </div>
           </div>
@@ -21,4 +20,3 @@ function Login() {
 
 }
 
-export default Login;
