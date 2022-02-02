3d1f15de00c6b67ad461b77d623292b5c0411238
Bryan Moore <31421559+bryanleemoore@users.noreply.github.com>
2022-02-01 18:58:02 -0800

    added responseFacebook, handleLogout (testing purpose), and TestScreen (testing purpose)

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -5,13 +5,38 @@ import {  LoginButton,getLocalUser,setLocalUser } from "./actions/login";
 
 
 
-function Login() {
+function Login() 
+{
+  const [currentLoginData, setLoginDataState] = useState(getLocalUser());
+  
+  const responseFacebook = async (response) => 
+  {
+    const userData = await response;
+    setLoginDataState(userData);
+    setLocalUser(userData);
+  };
+
+  const handleLogout = () => //testing purpose only
+  {
+    localStorage.removeItem('userLoginData');
+    setLoginDataState(null);
+  };
+
+const TestScreen = ({user}) => ( //testing purpose only
+  <>
+    <h1>Welcome {user.name} {user.id}!</h1>
+    <div><img src={user.picture.data.url} height={user.picture.height} width={user.picture.width} alt="avatar"/></div>
+    <button onClick={handleLogout}>Logout</button>
+  </>
+      )
+
   return (
       <div class="container">
         <div class="row align-items-center my-5">
           <div class="col-lg-7">
           <h1 class="font-weight-light">Sign In</h1>
             <p>
+            {currentLoginData ? (<TestScreen user = {currentLoginData}/>) : (<LoginButton responseFacebook = {responseFacebook}/>)}             
             </p>
           </div>
           </div>
@@ -19,4 +44,6 @@ function Login() {
   );
 
 }
+
+export default Login;
 
