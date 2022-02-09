b2f2cdee7a90bd622c2c869c856996bee39838d3
Shane Gilbert <35649752+shanedgilbert@users.noreply.github.com>
2022-02-09 10:40:50 -0800

    Separated parameters into variables

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,13 +4,17 @@ import StreamingAPIKey from './StreamingAPIKey';
 const DatabaseURL = 'http://localhost:5000/shows';
 const APIURL = 'https://streaming-availability.p.rapidapi.com/search/basic';
 const key = StreamingAPIKey();
+const pageNumber = 2;
+const streamingService = 'netflix';
+const contentType = 'movie';
 
 //Create a dropdown for service and type and send the data here
 const options = {
   params: {
     country: 'us',
-    service: 'hulu',
-    type: 'movie',
+    service: streamingService,
+    type: contentType,
+    page: pageNumber
   },
   headers: {
     'x-rapidapi-host': 'streaming-availability.p.rapidapi.com',
@@ -26,7 +30,11 @@ export const deleteShow = (id) => axios.delete(`${DatabaseURL}/${id}`);
 
 export const getShowsData = async () => {
   try {
-    const { data : { results } } = await axios.get(APIURL, options);
+    // const { data : { results } } = await axios.get(APIURL, options);
+    const { data } = await axios.get(APIURL, options);
+    // console.log(data.total_pages);
+    const { results } = data;
+    // console.log(results);
     return results;
   } catch(error) {
     console.log(error);
