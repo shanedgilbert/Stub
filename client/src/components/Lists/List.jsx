9536041390757627a599d724a71f5a554a2f06ce
LAPTOP-6ERV3FT1\britn <britneykhuu@gmail.com>
2022-02-10 15:53:32 -0800

    Button still wrong position; currently creating dropdown

--- client/src/components/Lists/List.jsx
+++ client/src/components/Lists/List.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './lists.css';
+import editButton from '../../images/edit-icon.png';
 
 function List(prop){
     return (
         <div class="listItem">
           <h3 class="listHeader">{prop.name}</h3>
+          <button class ="editButton" src ={editButton} ></button>
+          <div class="editMenu">
+            <a href="#">Edit Name</a>
+            <a href="#">Delete List</a>
+          </div>
         </div>
   );
 }
