7130822ad40ea7f6d5a5fa7bc46bb73d916390fa
Bryan Moore <31421559+bryanleemoore@users.noreply.github.com>
2022-02-15 01:45:35 -0800

    clean up login component and added a call to FacebookAPIKey()

--- client/src/components/jsx/Login.jsx
+++ client/src/components/jsx/Login.jsx
@@ -1,34 +1,50 @@
 import React,{ useState } from "react";
-import {  LoginButton,getLocalUser,setLocalUser } from "../../actions/login";
+import FacebookLogin from 'react-facebook-login'
+import {getLocalUser,setLocalUser,componentClicked } from "../../actions/login";
+import { createAccount } from '../../actions/accounts';
+import FacebookAPIKey from '../../api/FacebookAPIKey';
 import "../../index.css";
 
-function Login()
+const apiKey = FacebookAPIKey();
+
+
+
+function Login() 
 {
   const [currentLoginData, setLoginDataState] = useState(getLocalUser());
+  
 
-  const responseFacebook = async (response) =>
+  const responseFacebook = async (response) => 
   {
     const userData = await response;
     setLoginDataState(userData);
     setLocalUser(userData);
+    //console.log('test');
+    createAccount(response);
     window.location.reload();
   };
 
-  const handleLogout = () => //testing purpose only
-  {
-    localStorage.removeItem('userLoginData');
-    setLoginDataState(null);
-    window.location.reload();
-  };
+  const LoginButton = ({responseFacebook}) => (
+  
+    <FacebookLogin
+      appId={apiKey}
+      fields="name,email,picture"
+      onClick={componentClicked}
+      callback={responseFacebook}
+      icon="fa-facebook"/>
+    )
+
 
   return (
 
-            <h1>
-            {currentLoginData ? (<img id="loginImage" src={currentLoginData.picture.data.url} height={currentLoginData.picture.height} width={currentLoginData.picture.width} alt="avatar"/>) : (<LoginButton responseFacebook = {responseFacebook}/>)}             
+    <h1>
+    {currentLoginData ? (<img id="loginImage" src={currentLoginData.picture.data.url} height={currentLoginData.picture.height} width={currentLoginData.picture.width} alt="avatar"/>) : (<LoginButton responseFacebook = {responseFacebook}/>)}             
 
-        </h1>
+</h1>
   );
 
 }
 
 export default Login;
+
+
