fa633fd90ff98a2aa24fda3ff0d7408902dccd65
Bryan Moore <31421559+bryanleemoore@users.noreply.github.com>
2022-02-15 01:40:53 -0800

    Revert "cleaned up login component, added call to FacebookAPIKey()"
    This reverts commit c9cf491840c13c6831c6223b0aebdb59d0ead520.

--- client/src/components/jsx/Login.jsx
+++ client/src/components/jsx/Login.jsx
@@ -1,45 +1,30 @@
 import React,{ useState } from "react";
-import FacebookLogin from 'react-facebook-login'
-import {  LoginButton, getLocalUser,setLocalUser,componentClicked } from "../../actions/login";
-import { NavLink, useNavigate } from "react-router-dom";
-import { createAccount } from '../../actions/accounts';
-import FacebookAPIKey from '../../api/FacebookAPIKey';
+import {  LoginButton,getLocalUser,setLocalUser } from "../../actions/login";
+import "../../index.css";
 
-const apiKey = FacebookAPIKey();
-
-
-
-function Login() 
+function Login()
 {
   const [currentLoginData, setLoginDataState] = useState(getLocalUser());
-  
 
-  const responseFacebook = async (response) => 
+  const responseFacebook = async (response) =>
   {
     const userData = await response;
     setLoginDataState(userData);
     setLocalUser(userData);
-    //console.log('test');
-    createAccount(response);
     window.location.reload();
   };
 
-  const LoginButton = ({responseFacebook}) => (
-  
-    <FacebookLogin
-      appId={apiKey}
-      fields="name,email,picture"
-      onClick={componentClicked}
-      callback={responseFacebook}
-      icon="fa-facebook"/>
-    )
-
+  const handleLogout = () => //testing purpose only
+  {
+    localStorage.removeItem('userLoginData');
+    setLoginDataState(null);
+    window.location.reload();
+  };
 
   return (
 
             <h1>
-            {currentLoginData ? (<img src={currentLoginData.picture.data.url} height={currentLoginData.picture.height} width={currentLoginData.picture.width} alt="avatar"/>) : 
-            (<LoginButton responseFacebook = {responseFacebook}/>)}             
+            {currentLoginData ? (<img id="loginImage" src={currentLoginData.picture.data.url} height={currentLoginData.picture.height} width={currentLoginData.picture.width} alt="avatar"/>) : (<LoginButton responseFacebook = {responseFacebook}/>)}             
 
         </h1>
   );
