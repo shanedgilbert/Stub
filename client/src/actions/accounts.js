e82fda74b661cbe9cd40ea8d75d3cd88a4a9ca94
Bryan Moore <31421559+bryanleemoore@users.noreply.github.com>
2022-02-15 01:26:06 -0800

    added actions for accounts

---
+++ client/src/actions/accounts.js
@@ -0,0 +1,30 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE, RATE } from '../constants/actionTypes';
+
+import * as api from '../api/index.js';
+
+export async function getAccounts() 
+{
+    try 
+    {
+        await fetch(api.fetchAccounts(), {method: FETCH_ALL});
+    }
+   catch (error) 
+    {
+        console.log(error.message);
+    }
+};
+
+export async function createAccount(account)
+{
+ 
+  //console.log(account);
+  try 
+  {
+    await fetch(api.createAccount(account), {method: CREATE});
+
+  } 
+  catch (error) 
+  {
+    console.log(error.message);
+  }
+}
