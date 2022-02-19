c6a9ae4e211f9227b974d6d475ae7801d2430dcb
Bryan Moore <31421559+bryanleemoore@users.noreply.github.com>
2022-02-19 01:24:56 -0800

    added "/logout" route which navigates to homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import { Navigation, Footer, Home, Lists, Settings } from "./components/jsx";
 import { isLoggedIn } from './actions/login';
 import PrivateRoute from './components/jsx/PrivateRoute';
@@ -11,6 +11,7 @@ const App = () => {
       <Navigation LoggedInState={LoggedIn} />
       <Routes>
         <Route path="/" element={<Home />} /> {/*Test */}
+        <Route path="/logout" element={<Navigate to="/" />} />
         <Route
           path="/settings"
           element=
