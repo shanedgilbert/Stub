c1eebd7f7fbe0de9f3648d1ed8999984b904f221
Bryan Moore <31421559+bryanleemoore@users.noreply.github.com>
2022-02-19 02:14:42 -0800

    add logout

--- client/src/components/jsx/index.js
+++ client/src/components/jsx/index.js
@@ -3,3 +3,4 @@ export { default as Footer } from "./Footer";
 export { default as Home } from "./Home";
 export { default as Lists } from "./Lists";
 export { default as Settings } from "./Settings";
+export { default as Logout } from "./Logout";
