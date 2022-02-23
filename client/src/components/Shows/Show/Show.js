4955748af7824a8829bb57d72c67538ebcf8d40f
Shane Gilbert <35649752+shanedgilbert@users.noreply.github.com>
2022-02-22 16:10:19 -0800

    Modal is visible when clicking card

--- client/src/components/Shows/Show/Show.js
+++ client/src/components/Shows/Show/Show.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core/';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import Modal from 'react-bootstrap/Modal'
@@ -12,12 +12,13 @@ const Show = ({ show }) => {
   const classes = useStyles();
   const imdbRatingNormalized = show.imdbRating/10;
 
-  function handleCardClick(event) {
-    console.log("clicked!")
-  }
+  const [visibility, setVisibility] = useState(false);
+
+  const handleCloseModal = () => setVisibility(false);
+  const handleShowModal = () => setVisibility(true);
 
   return (
-      <Card className={classes.card} onClick={handleCardClick}>
+      <Card className={classes.card} onClick={handleShowModal}>
         <CardMedia className={classes.media} image={show.posterURLs.original || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={show.title} />
         <div className={classes.overlay}>
           <Typography variant="h6">{show.originalTitle}</Typography>
@@ -29,6 +30,16 @@ const Show = ({ show }) => {
         <div className={classes.overlay2}>
           {/* <Button style={{ color: 'white' }} size="small" onClick={() => setCurrentId(show._id)}><MoreHorizIcon fontSize="default" /></Button> */}
         </div>
+        <Modal className={classes.modal} show={visibility} onHide={handleCloseModal}>
+          <Modal.Header>
+            <Modal.Title>
+              <Typography variant="h6">{show.originalTitle}</Typography>
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Test
+          </Modal.Body>
+        </Modal>
         {/* <div className={classes.details}>
           <Typography variant="body2" color="textSecondary" component="h2">{show.cast.map((cast) => `${cast}, `)}</Typography>
         </div>
--- client/src/components/Shows/Show/styles.js
+++ client/src/components/Shows/Show/styles.js
@@ -61,5 +61,8 @@ export default makeStyles({
     padding: '0 16px 8px 16px',
     display: 'flex',
     justifyContent: 'space-between',
+  },
+  modal: {
+    color: 'black',
   }
 });
