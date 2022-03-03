869ab5f18a530d035e12fcbc4e4ba5d38ca58da6
scottsak <48599109+scottsak@users.noreply.github.com>
2022-03-03 00:59:08 -0800

    updated button layout

--- client/src/components/Lists/List.jsx
+++ client/src/components/Lists/List.jsx
@@ -7,21 +7,23 @@ import {deleteList} from '../../actions/lists.js';
 function List(prop){
 
   const dispatch = useDispatch();
-  
+
   const handleDelete = (payload) => {
     console.log("LIST.JSX: " + payload);
     dispatch(deleteList(payload));
   }
 
     return (
-        <div>
-          <h3 class="listHeader">{prop.name}</h3>
-          <div class="listItem">
-            <div class="dropdown">
-              <button class ="editButton">dropdown</button>
-              <div class="editMenuContent">
-                <button class="dropdownLink" onClick={ListUpdate.handle_submit}>Edit</button>
-                <button class="dropdownLink" onClick={() => handleDelete(prop._id)}>Delete</button>
+        <div className="listButton">
+          <div className="listHeaderDiv">
+          <h3 className="listHeader">{prop.name}</h3>
+          </div>
+          <div className="listItem">
+            <div className="dropdown">
+              <button className ="editButton">dropdown</button>
+              <div className="editMenuContent">
+                <button className="dropdownLink" onClick={ListUpdate.handle_submit}>Edit</button>
+                <button className="dropdownLink" onClick={() => handleDelete(prop._id)}>Delete</button>
               </div>
             </div>
           </div>
