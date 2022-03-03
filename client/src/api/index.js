88842613e47cb2396e8c0f0bc2dae16b56e09ee4
Bryan Moore <31421559+bryanleemoore@users.noreply.github.com>
2022-03-03 02:09:21 -0800

    update api call

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -6,23 +6,11 @@ const DatabaseURLAccounts = 'http://localhost:5000/accounts';
 const DatabaseURLLists = 'http://localhost:5000/listsdb';
 const APIURL = 'https://streaming-availability.p.rapidapi.com/search/basic';
 const key = StreamingAPIKey();
-const pageNumber = 2;
 const streamingService = 'netflix';
 const contentType = 'movie';
 
 //Create a dropdown for service and type and send the data here
-const options = {
-  params: {
-    country: 'us',
-    service: streamingService,
-    type: contentType,
-    page: pageNumber
-  },
-  headers: {
-    'x-rapidapi-host': 'streaming-availability.p.rapidapi.com',
-    'x-rapidapi-key': key,
-  }
-};
+
 
 export const fetchShows = () => axios.get(DatabaseURL);
 export const createShow = (newShow) => axios.post(DatabaseURL, newShow);
@@ -30,12 +18,27 @@ export const rateShow = (id) => axios.patch(`${DatabaseURL}/${id}/rateShow`);
 export const updateShow = (id, updatedShow) => axios.patch(`${DatabaseURL}/${id}`, updatedShow);
 export const deleteShow = (id) => axios.delete(`${DatabaseURL}/${id}`);
 
-export const getShowsData = async () => {
+export const getShowsData = async (pageNumber) => {
+  const options = {
+    params: {
+      country: 'us',
+      service: streamingService,
+      type: contentType,
+      page: pageNumber
+    },
+    headers: {
+      'x-rapidapi-host': 'streaming-availability.p.rapidapi.com',
+      'x-rapidapi-key': key,
+    }
+  };
+
   try {
+ 
     // const { data : { results } } = await axios.get(APIURL, options);
     const { data } = await axios.get(APIURL, options);
     // console.log(data.total_pages);
     const { results } = data;
+  
     console.log(results);
     // console.log(results);
     return results;
