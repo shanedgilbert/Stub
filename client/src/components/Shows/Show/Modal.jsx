49c6e1ad1d559afc6e4779983fcc02aea01b2dee
scottsak <48599109+scottsak@users.noreply.github.com>
2022-03-10 18:48:00 -0800

    added modal features

--- client/src/components/Shows/Show/Modal.jsx
+++ client/src/components/Shows/Show/Modal.jsx
@@ -1,15 +1,64 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import { CSSTransition } from "react-transition-group";
+import "./modal.css";
 
 
-function Modal(props) {
-  return (
-    <div class="modal" id="modal">
-      <h2>Modal Window</h2>
-      <div class="content">hello</div>
-      <div class="actions">
+const Modal = props => {
+  const closeOnEscapeKeyDown = e => {
+    if ((e.charCode || e.keyCode) === 27) {
+      props.onClose();
+    }
+  };
+
+  useEffect(() => {
+    document.body.addEventListener("keydown", closeOnEscapeKeyDown);
+    return function cleanup() {
+      document.body.removeEventListener("keydown", closeOnEscapeKeyDown);
+    };
+  }, []);
+
+  return ReactDOM.createPortal(
+    <CSSTransition
+      in={props.show}
+      unmountOnExit
+      timeout={{ enter: 0, exit: 300 }}
+    >
+      <div className="modal" onClick={props.onClose}>
+        <div className="modal-content" onClick={e => e.stopPropagation()}>
+          <div className="modal-header">
+
+          </div>
+
+          <div className="modal-body">
+
+          <div>
+            <button onClick={props.onClose} className="button closeButton">
+              x
+            </button>
+            <img src={props.movieImage} alt={props.title} width="899px"></img>
+
+          </div>
+
+          <tr className="modalTable">
+            <td className="modalOverview">
+              <h2 className="modal-title">{props.title}</h2>
+              <p>Release Date: {props.date} Runtime: {props.runtime} minutes</p>
+              <p>{props.overview}</p>
+            </td>
+            <td className="modalInfo">
+              <p>cast: {props.cast}</p>
+            </td>
+            </tr>
+          <p>{props.children}</p>
+          </div>
+          <div className="modal-footer">
+            </div>
+        </div>
       </div>
-    </div>
+    </CSSTransition>,
+    document.getElementById("root")
   );
-}
+};
 
 export default Modal;
--- client/src/components/Shows/Show/Show.js
+++ client/src/components/Shows/Show/Show.js
@@ -1,6 +1,6 @@
 import React, { useState, setState } from 'react';
 import { Card, CardActions, CardContent, CardMedia, Typography } from '@material-ui/core/';
-import { Modal } from 'react-bootstrap'
+import  Modal  from './Modal.jsx'
 import { useDispatch } from 'react-redux';
 
 import useStyles from './styles';
@@ -13,15 +13,17 @@ const Show = ({ show }) => {
   const [visibility, setVisibility] = useState(false);
 
   const handleCloseModal = () => {
+    console.log("went through")
     setVisibility(false);
   }
   const handleShowModal = () =>  {
+    console.log("did not go through")
     setVisibility(true);
   }
 
   return (
       <Card className={classes.card}>
-        <CardMedia className={classes.media} onClick={handleShowModal} image={show.posterURLs.original || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={show.title} />
+        <CardMedia className={classes.media} onClick={() => setVisibility(true)} image={show.posterURLs.original || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={show.title} />
         <div className={classes.overlay}>
           <Typography variant="h6">{show.originalTitle}</Typography>
           <Typography variant="body2">{show.year}</Typography>
@@ -32,22 +34,15 @@ const Show = ({ show }) => {
         <div className={classes.overlay2}>
           {/* <Button style={{ color: 'white' }} size="small" onClick={() => setCurrentId(show._id)}><MoreHorizIcon fontSize="default" /></Button> */}
         </div>
-        <Modal className={classes.modal, classes.lengthofthing} show={visibility} onHide={handleCloseModal} backdrop={true}>
-          <Modal.Header className={classes.modal} closeButton>
-
-            <Modal.Title className={classes.modal}>
-              <Typography variant="h6">{show.originalTitle}</Typography>
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body className={classes.modal}>
-            <img src={show.backdropURLs.original || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} alt="poster" width="2048" className="img-fluid"/>
-            <Typography variant="body2" component="h2">{show.cast.map((cast) => `${cast}, `)}</Typography>
-          </Modal.Body>
-          {/* <Modal.Body>
-            <Typography variant="body2" color="textSecondary" component="p">{show.overview}</Typography> */}
-            {/* <Typography className={classes.title} variant="h5" component="h2">{show.tagline}</Typography> */}
-          {/* </Modal.Body> */}
-        </Modal>
+        <Modal onClose={() => setVisibility(false)}
+        show={visibility}
+        date= {show.year}
+        runtime = {show.runtime}
+        title= {show.originalTitle}
+        movieImage = {show.backdropURLs.original}
+        overview = {show.overview}
+        cast = {show.cast}
+        />
       </Card>
   );
 };
